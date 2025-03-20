65f3b379931dd5018f44055d2d484997b5b8f420
Nyrek2001 <Nyrekwheeler@gmail.com>
2025-03-20 11:45:00 -0400

    Add the stack function

---
+++ supplementproj2_3/target/debug/deps/supplementproj2_3.d
@@ -0,0 +1,5 @@
+C:\Users\mcelr\supplementproj2_3\target\debug\deps\supplementproj2_3.exe: src\main.rs
+
+C:\Users\mcelr\supplementproj2_3\target\debug\deps\supplementproj2_3.d: src\main.rs
+
+src\main.rs:
---
+++ supplementproj2_3/target/debug/supplementproj2_3.d
@@ -0,0 +1,1 @@
+C:\Users\mcelr\supplementproj2_3\target\debug\supplementproj2_3.exe: C:\Users\mcelr\supplementproj2_3\src\main.rs
