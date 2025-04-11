81cab7d4d63e5ac212c414f553aa539c0c420cf7
NyrekWheeler <142206465+Nyrek2001@users.noreply.github.com>
2025-04-10 22:15:34 -0400

    Delete supplement_2_5 directory

--- supplement_2_5/target/debug/deps/supplement_2_5-ac665337ec710835.d
+++
@@ -1,7 +0,0 @@
-C:\Users\mcelr\supplement_2_5\target\debug\deps\libsupplement_2_5-ac665337ec710835.rmeta: src\lib.rs
-
-C:\Users\mcelr\supplement_2_5\target\debug\deps\libsupplement_2_5-ac665337ec710835.rlib: src\lib.rs
-
-C:\Users\mcelr\supplement_2_5\target\debug\deps\supplement_2_5-ac665337ec710835.d: src\lib.rs
-
-src\lib.rs:
--- supplement_2_5/target/debug/libsupplement_2_5.d
+++
@@ -1,1 +0,0 @@
-C:\Users\mcelr\supplement_2_5\target\debug\libsupplement_2_5.rlib: C:\Users\mcelr\supplement_2_5\src\lib.rs
