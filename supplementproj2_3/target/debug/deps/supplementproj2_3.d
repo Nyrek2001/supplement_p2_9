a3876556e92cfe579689b8e083b820c88a8f7a52
NyrekWheeler <142206465+Nyrek2001@users.noreply.github.com>
2025-04-10 22:15:05 -0400

    Delete supplementproj2_3 directory

--- supplementproj2_3/target/debug/deps/supplementproj2_3.d
+++
@@ -1,5 +0,0 @@
-C:\Users\mcelr\supplementproj2_3\target\debug\deps\supplementproj2_3.exe: src\main.rs
-
-C:\Users\mcelr\supplementproj2_3\target\debug\deps\supplementproj2_3.d: src\main.rs
-
-src\main.rs:
--- supplementproj2_3/target/debug/supplementproj2_3.d
+++
@@ -1,1 +0,0 @@
-C:\Users\mcelr\supplementproj2_3\target\debug\supplementproj2_3.exe: C:\Users\mcelr\supplementproj2_3\src\main.rs
